5df19557a39aaf693d6f5c963dd5d89d64dbad79
Cosmas <cosmasnnabuike@gmail.com>
2023-07-14 06:20:39 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -8,6 +8,7 @@
 int main(void)
 {
 	char c;
+
 	c = 'a';
 	while
 		(c <= 'z'){
