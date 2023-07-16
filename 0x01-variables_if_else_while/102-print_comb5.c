e3c7ec8617dfa91945b85e9c314d613cd0154ea7
Cosmas <cosmasnnabuike@gmail.com>
2023-07-16 09:18:20 -0700

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
 /**
- * main - print numbers in 3s separaed by , and space
+ * main - print 4 digits separated by space and comma
  *
  * Return: (0)
  *
  */
 int main(void)
 {
-	int num1, num2, num3;
+	int i, j;
+	int a, b, c, d;
 
-	num1 = '0';
-	num2 = '0';
-	num3 = '0';
-
-	while (num1 <= '9')
+	for (i = 0; i < 100; i++)
 	{
-		while (num2 <= '9')
+		a = i / 10;
+		b = i % 10;
+
+		for (j = 0; j < 100; j++)
 		{
-			num = '0';
-			while (num3 <= '9')
+			c = j / 10;
+			b = j % 10;
+
+			if (a < c || (a == c && b < d))
 			{
-				if (num1 < num2 && num2 < num3)
-				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
 
-					if (num1 != '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				num3++;
-			}
-			num2++;
-		}
-		num1++;
-		num2 = '0';
-	}
-	putchar('\n');
-
-	return (0);
-}
+				if (!(a == 9 && b == 8))
